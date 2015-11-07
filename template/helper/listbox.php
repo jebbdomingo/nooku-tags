745241c4844e936b8d2c12ece947606f4f22f45f
Rastin Mehr <rastin@timble.net>
2015-11-07 00:12:05 +0000

    issue #1 added tag list form field

---
+++ template/helper/listbox.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Nooku Platform - http://www.nooku.org/platform
+ *
+ * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/nooku/nooku-platform for the canonical source repository
+ */
+
+/**
+ * Listbox Template Helper
+ *
+ * @author  Tom Janssens <http://github.com/tomjanssens>
+ * @package Component\Tags
+ */
+class ComTagsTemplateHelperListbox extends KTemplateHelperListbox
+{
+    public function tags($config = array())
+    {
+        $config = new KObjectConfig($config);
+
+        $config->append(array(
+            'model' => 'tags',
+            'value'	 => 'id',
+            'label'	 => 'title',
+            'prompt' => false,
+            'select2' => true,
+            'identifier' => 'com:tags.model.tags'
+        ));
+
+        $config->label = 'title';
+        $config->sort  = 'title';
+
+        return parent::_render($config);
+    }
+}
